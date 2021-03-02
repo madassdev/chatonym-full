92d3954b06085e66a856df85aebadc5580d2a601
lordfaves <favescsskr@gmail.com>
2021-03-02 15:02:14 +0100

    styles

--- resources/views/interface/index.blade.php
+++ resources/views/interface/index.blade.php
@@ -11,10 +11,6 @@
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@100;200;300;400;500;600;700;800;900&display=swap"
             rel="stylesheet"
         />
-        <link
-            rel="stylesheet"
-            href="https://cdn.materialdesignicons.com/5.4.55/css/materialdesignicons.min.css"
-        />
 
         <title>Chatonym</title>
     </head>
