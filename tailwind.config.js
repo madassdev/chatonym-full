89635f5f35c747cfb97b8284d7e74f9459115f97
lordfaves <favescsskr@gmail.com>
2021-04-02 18:21:36 +0100

    Production build

--- tailwind.config.js
+++ tailwind.config.js
@@ -16,8 +16,8 @@ module.exports = {
         extend: {
             colors: {
                 "cha-primary": "#6A50D3",
-                // "cha-secondary": "#F9F8FF",
-                "cha-secondary": "#dddbeb",
+                "cha-secondary": "#F9F8FF",
+                // "cha-secondary": "#dddbeb",
                 "cha-light": "#F9F8FF",
                 "cha-accent": "#A7D2F1",
             },
